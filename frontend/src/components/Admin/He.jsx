71560925c4203a60435e34d9dda14ea33619dead
SUREDDY KAVYASRI <2100030517cseh@gmail.com>
2024-06-15 16:30:15 +0530

    iuyt

--- frontend/src/components/Admin/He.jsx
+++ frontend/src/components/Admin/He.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function He() {
   return (
-    <div>He</div>
+    <div>He
+      <h4>iuygtfd</h4>
+      <h5>uygfd</h5>
+      <h6>uytrfd</h6>
+    </div>
+    
   )
 }
 
